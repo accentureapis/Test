89f78d1608698a383e25e3aec0f060c439238300
accentureapis <31919314+accentureapis@users.noreply.github.com>
2017-11-17 12:44:47 +0530

    Update DataSource.scala

--- src/main/scala/DataSource.scala
+++ src/main/scala/DataSource.scala
@@ -33,14 +33,14 @@ class DataSource(ep: EmptyParams)
     val grades = PEventStore.aggregateProperties(
       appName = sys.env("PIO_EVENTSERVER_APP_NAME"),
       entityType = "student",
-      required = Some(List("item","quantity","user","day","month"))
+      required = Some(List("item","quantity","vendor","day","month"))
     )(sc)
 
     val events = grades.map {
       case (entityId, properties) =>
         val fields = Map(
           "vector" -> JArray(List(
-             JDouble(properties.get[Double]("item")),JDouble(properties.get[Double]("user")),JDouble(properties.get[Double]("day")),JDouble(properties.get[Double]("month"))
+             JDouble(properties.get[Double]("item")),JDouble(properties.get[Double]("vendor")),JDouble(properties.get[Double]("day")),JDouble(properties.get[Double]("month"))
            )),
 		   "label" -> JDouble(properties.get[Double]("quantity"))
         )
