4d368c6b2ec2a639fe8213b7cf0859acd7a55d88
accentureapis <31919314+accentureapis@users.noreply.github.com>
2017-12-06 15:58:17 +0530

    Update Preparator.scala

--- src/main/scala/Preparator.scala
+++ src/main/scala/Preparator.scala
@@ -5,10 +5,16 @@ import org.apache.predictionio.data.storage.PropertyMap
 import org.apache.spark.SparkContext
 import org.apache.spark.rdd.RDD
 
+class PreparedData(
+   val labeledPoints: RDD[LabeledPoint],
+   val gendersMap: Map[String,Double],
+   val educationMap: Map[String,Double]
+ ) extends Serializable
+
 class Preparator extends PPreparator[TrainingData, PreparedData] {
   def prepare(sc: SparkContext, trainingData: TrainingData): PreparedData = {
     PreparedData(trainingData.values)
   }
 }
 
-case class PreparedData(values: RDD[(String, PropertyMap)])
+//case class PreparedData(values: RDD[(String, PropertyMap)])
