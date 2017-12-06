f0dd14040b8a88b881623ac1ad2495b05faca9bc
accentureapis <31919314+accentureapis@users.noreply.github.com>
2017-12-06 15:56:58 +0530

    Update Engine.scala

--- src/main/scala/Engine.scala
+++ src/main/scala/Engine.scala
@@ -27,32 +27,25 @@ object RegressionEngine extends EngineFactory {
   }
 }
 */
-package org.apache.predictionio.examples.classification
-
-import org.apache.predictionio.controller.EngineFactory
-import org.apache.predictionio.controller.Engine
-
-case class Query(
-  attr0 : Double,
-  attr1 : Double,
-  attr2 : Double
-)
-
-case class PredictedResult(
-  label: Double
-)
-
-case class ActualResult(
-  label: Double
-)
-
-object ClassificationEngine extends EngineFactory {
-  def apply() = {
-    new Engine(
-      classOf[DataSource],
-      classOf[Preparator],
-      Map("naive" -> classOf[NaiveBayesAlgorithm],
-        "randomforest" -> classOf[RandomForestAlgorithm]), // ADDED
-      classOf[Serving])
-  }
-}
+  import io.prediction.controller.IEngineFactory
+ import io.prediction.controller.Engine
+ 
+ class Query(
+  val  gender: String,
+  val  age: Int,
+  val  education: String
+ ) extends Serializable
+ 
+ class PredictedResult(
+   val label: Double
+ ) extends Serializable
+ 
+ object ClassificationEngine extends IEngineFactory {
+   def apply() = {
+     new Engine(
+       classOf[DataSource],
+       classOf[Preparator],
+       Map("randomforest" -> classOf[RandomForest]),
+       classOf[Serving])
+   }
+ }
