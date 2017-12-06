408ada52f4ad6f70c225e1234316942abc7e0992
accentureapis <31919314+accentureapis@users.noreply.github.com>
2017-12-06 12:57:19 +0530

    Update Engine.scala

--- src/main/scala/Engine.scala
+++ src/main/scala/Engine.scala
@@ -1,4 +1,4 @@
-package org.template.regression
+/*package org.template.regression
 
 import org.apache.predictionio.controller.{EmptyEvaluationInfo, Engine, EngineFactory}
 
@@ -24,5 +24,35 @@ object RegressionEngine extends EngineFactory {
       ),
       classOf[Serving]
     )
+  }
+}
+*/
+package org.apache.predictionio.examples.classification
+
+import org.apache.predictionio.controller.EngineFactory
+import org.apache.predictionio.controller.Engine
+
+case class Query(
+  attr0 : Double,
+  attr1 : Double,
+  attr2 : Double
+)
+
+case class PredictedResult(
+  label: Double
+)
+
+case class ActualResult(
+  label: Double
+)
+
+object ClassificationEngine extends EngineFactory {
+  def apply() = {
+    new Engine(
+      classOf[DataSource],
+      classOf[Preparator],
+      Map("naive" -> classOf[NaiveBayesAlgorithm],
+        "randomforest" -> classOf[RandomForestAlgorithm]), // ADDED
+      classOf[Serving])
   }
 }
