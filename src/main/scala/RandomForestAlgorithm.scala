de416ace9bbaed9e3ec536ebfa432c40963d218b
accentureapis <31919314+accentureapis@users.noreply.github.com>
2017-12-06 14:10:00 +0530

    Update RandomForestAlgorithm.scala

--- src/main/scala/RandomForestAlgorithm.scala
+++ src/main/scala/RandomForestAlgorithm.scala
@@ -1,70 +1,64 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-package org.apache.predictionio.examples.classification
-
-import org.apache.predictionio.controller.P2LAlgorithm
-import org.apache.predictionio.controller.Params
-
-import org.apache.spark.mllib.tree.RandomForest // CHANGED
-import org.apache.spark.mllib.tree.model.RandomForestModel // CHANGED
-import org.apache.spark.mllib.linalg.Vectors
-import org.apache.spark.SparkContext
-
-// CHANGED
-case class RandomForestAlgorithmParams(
-  numClasses: Int,
-  numTrees: Int,
-  featureSubsetStrategy: String,
-  impurity: String,
-  maxDepth: Int,
-  maxBins: Int
-) extends Params
-
-// extends P2LAlgorithm because the MLlib's RandomForestModel doesn't
-// contain RDD.
-class RandomForestAlgorithm(val ap: RandomForestAlgorithmParams) // CHANGED
-  extends P2LAlgorithm[PreparedData, RandomForestModel, // CHANGED
-  Query, PredictedResult] {
-
-  // CHANGED
-  def train(sc: SparkContext, data: PreparedData): RandomForestModel = {
-    // CHANGED
-    // Empty categoricalFeaturesInfo indicates all features are continuous.
-    val categoricalFeaturesInfo = Map[Int, Int]()
-    RandomForest.trainClassifier(
-      data.labeledPoints,
-      ap.numClasses,
-      categoricalFeaturesInfo,
-      ap.numTrees,
-      ap.featureSubsetStrategy,
-      ap.impurity,
-      ap.maxDepth,
-      ap.maxBins)
-  }
-
-  def predict(
-    model: RandomForestModel, // CHANGED
-    query: Query): PredictedResult = {
-
-    val label = model.predict(Vectors.dense(
-      Array(query.attr0, query.attr1, query.attr2)
-    ))
-    PredictedResult(label)
-  }
-
-}
+ import io.prediction.controller.PDataSource
+ import io.prediction.controller.EmptyEvaluationInfo
+ import io.prediction.controller.EmptyActualResult
+ import io.prediction.controller.Params
+ import io.prediction.data.storage.Event
+ import io.prediction.data.storage.Storage
+ 
+ import org.apache.spark.SparkContext
+ import org.apache.spark.SparkContext._
+ import org.apache.spark.rdd.RDD
+ import org.apache.spark.mllib.regression.LabeledPoint
+ import org.apache.spark.mllib.linalg.Vectors
+ 
+ import grizzled.slf4j.Logger
+ 
+ case class DataSourceParams(appId: Int) extends Params
+ 
+ class DataSource(val dsp: DataSourceParams)
+   extends PDataSource[TrainingData,
+       EmptyEvaluationInfo, Query, EmptyActualResult] {
+ 
+   @transient lazy val logger = Logger[this.type]
+ 
+   override
+   def readTraining(sc: SparkContext): TrainingData = {
+     val eventsDb = Storage.getPEvents()
+     val gendersMap = Map("Male" -> 0.0, "Female" -> 1.0)
+     val educationMap = Map("No School" -> 0.0,"High School" -> 1.0,"College" -> 2.0)
+     val labeledPoints: RDD[LabeledPoint] = eventsDb.aggregateProperties(
+       appId = dsp.appId,
+       entityType = "user",
+       // only keep entities with these required properties defined
+       required = Some(List("plan", "gender", "age", "education")))(sc)
+       // aggregateProperties() returns RDD pair of
+       // entity ID and its aggregated properties
+       .map { case (entityId, properties) =>
+         try {
+           LabeledPoint(properties.get[Double]("plan"),
+             Vectors.dense(Array(
+               gendersMap(properties.get[String]("gender")),
+               properties.get[Double]("age"),
+               educationMap(properties.get[String]("education"))
+             ))
+           )
+         } catch {
+           case e: Exception => {
+             logger.error(s"Failed to get properties ${properties} of" +
+               s" ${entityId}. Exception: ${e}.")
+             throw e
+           }
+         }
+       }.cache()
+ 
+     new TrainingData(labeledPoints,
+ 	gendersMap,
+ 	educationMap)
+   }
+ }
+ 
+ class TrainingData(
+   val labeledPoints: RDD[LabeledPoint],
+   val gendersMap: Map[String,Double],
+   val educationMap: Map[String,Double]
+ ) extends Serializable
